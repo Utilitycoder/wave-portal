4a0881bafa9294ea5e6d8a8cbdd075f0b283bc6a
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-01-02 11:11:23 +0100

    Update repo

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     matic: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/z5Y23IoLzFP0Vdq3ZkGvxRsTHAHKK1Tb",
-      accounts: [""],
+      url: process.env.MATIC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
 
 // 0x6ABaEe57A542542B9087808230E370a24aCc8C37
-
-<div className="bio">
-I am Lawal. I am a budding Dapp developer. This is one of my numerous dapps. Kindly interact and you could even win a price.
-<p>Wave and send message. Thanks</p>
-  </div>
