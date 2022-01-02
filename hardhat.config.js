770266feb2783f983679e58c09e4266a81ea776a
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-01-02 10:20:51 +0100

    Updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,14 @@ module.exports = {
   networks: {
     matic: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/z5Y23IoLzFP0Vdq3ZkGvxRsTHAHKK1Tb",
-      accounts: ["2cf0261644a795ce2afa01b14f1c0b8bbcc0717cc8b47956e7e291208b4c9851"],
+      accounts: [""],
     },
   },
 };
 
 // 0x6ABaEe57A542542B9087808230E370a24aCc8C37
+
+<div className="bio">
+I am Lawal. I am a budding Dapp developer. This is one of my numerous dapps. Kindly interact and you could even win a price.
+<p>Wave and send message. Thanks</p>
+  </div>
